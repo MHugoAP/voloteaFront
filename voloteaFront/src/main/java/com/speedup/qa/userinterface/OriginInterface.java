465a078f1420c17b35cc3fd31e8274b227ed94d9
Miguel Hugo <dragonmike6@gmail.com>
2022-09-08 16:05:20 -0500

    added new locators to implement them

--- voloteaFront/src/main/java/com/speedup/qa/userinterface/OriginInterface.java
+++ voloteaFront/src/main/java/com/speedup/qa/userinterface/OriginInterface.java
@@ -1,5 +1,6 @@
 package com.speedup.qa.userinterface;
 
+import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
@@ -21,6 +22,11 @@ public class OriginInterface {
             .the("Write the origin")
             .located(By.className("v7-sf__input v7-is-focused"));
 
+    public static final By ENTER_BY_GROUP = By.className("v7-rounded v7-rounded--feedback");
+
+    public static final Target ENTER_BY_GROUP_TARGET = Target
+            .the("validate that groups can be booked")
+            .located(By.className("v7-rounded v7-rounded--feedback"));
     public static final By VALIDATE_NUMBER_ADULT = By.xpath("(//*[@class='v7-passenger__number ng-star-inserted'])[1]");
 
     public static final By VALIDATE_NUMBER_CHILD = By.xpath("(//*[@class='v7-passenger__number ng-star-inserted'])[2]");

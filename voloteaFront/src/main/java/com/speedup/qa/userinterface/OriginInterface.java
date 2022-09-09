753b4c973a240d153d7052758136d0225bde8fce
Miguel Hugo <dragonmike6@gmail.com>
2022-09-09 15:48:16 -0500

    locators changed

--- voloteaFront/src/main/java/com/speedup/qa/userinterface/OriginInterface.java
+++ voloteaFront/src/main/java/com/speedup/qa/userinterface/OriginInterface.java
@@ -12,14 +12,20 @@ public class OriginInterface {
     public static final Target CLIC_WRITE_ORIGIN = Target
             .the("Write the origin")
             .located(By.xpath("//input[@id='origin']"));
+    public static final By CLIC_WRITE_ORIGIN_VERSION2 = By.xpath("//input[@id='origin']");
+
+    public static final Target CLIC_WRITE_ORIGIN_VERSION3 = Target
+            .the("Write the origin")
+            .located(By.className("//input[@id='origin']"));
 
     public static final Target WRITE_ORIGIN = Target
             .the("Write the origin")
             .located(By.className("v7-sf__input v7-is-focused"));
 
     public static final Target CLIC_WRITE_DESTINY = Target
-            .the("Write the origin")
+            .the("Write the destiny")
             .located(By.xpath("//input[@id='destination']"));
+    public static final By CLIC_WRITE_DESTINY_VERSION2 = By.xpath("//input[@id='destination']");
 
     public static final Target WRITE_DESTINY = Target
             .the("Write the origin")
@@ -30,6 +36,15 @@ public class OriginInterface {
     public static final Target ENTER_BY_GROUP_TARGET = Target
             .the("validate that groups can be booked")
             .located(By.xpath("//span[@class='v7-rounded__text v7-rounded__text--xs']"));
+
+    public static final By VALIDATE_ORIGIN = By.className("v7-sf__input v7-is-focused");
+
+    public static final By VALIDATE_DESTINY = By.xpath("(//input[@class='v7-sf__input'])[2]");
+
+    public static final Target NOT_FIND_A_SEARCH_ORIGIN = Target
+            .the("We do not find a search")
+            .located(By.className("v7-route v7-route--no-results || v7-js-route"));
+
     public static final By VALIDATE_NUMBER_ADULT = By.xpath("(//*[@class='v7-passenger__number ng-star-inserted'])[1]");
 
     public static final By VALIDATE_NUMBER_CHILD = By.xpath("(//*[@class='v7-passenger__number ng-star-inserted'])[2]");

4356ce4e6dfdf28d77df034bfb182fe439d45be7
Miguel Hugo <dragonmike6@gmail.com>
2022-09-08 19:47:36 -0500

    we validate the select button by groups

--- voloteaFront/src/main/java/com/speedup/qa/questions/SelectGroup.java
+++ voloteaFront/src/main/java/com/speedup/qa/questions/SelectGroup.java
@@ -3,18 +3,17 @@ package com.speedup.qa.questions;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.questions.Text;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
-import static com.speedup.qa.userinterface.OriginInterface.ENTER_BY_GROUP;
+import static com.speedup.qa.userinterface.OriginInterface.ENTER_BY_GROUP_TARGET;
 
-public class SelectGroup implements Question<Boolean> {
+public class SelectGroup implements Question<String> {
     @Override
-    public Boolean answeredBy(Actor actor) {
-        return BrowseTheWeb.as(actor).find(ENTER_BY_GROUP).isVisible();
-        /*if(dynamicElement.size() != 0){
-            System.out.println("Existe elemento");
-        }else{//0, elemento no esta presente.
-            System.out.println("Elemento no existe");
-        }*/
+    public String answeredBy(Actor actor) {
+        actor.attemptsTo(WaitUntil.the((ENTER_BY_GROUP_TARGET), WebElementStateMatchers.isVisible()));
+        return Text.of(ENTER_BY_GROUP_TARGET).viewedBy(actor).asString();
     }
     public static SelectGroup messageSelectGroup(){
         return new SelectGroup();

cea62bd8737a1b3dff85dd803bdf6e672ee89413
Miguel Hugo <dragonmike6@gmail.com>
2022-09-08 19:46:20 -0500

    added new functionality in the then

--- voloteaFront/src/test/java/com/speedup/qa/stepdefinition/passagerStepDefinition.java
+++ voloteaFront/src/test/java/com/speedup/qa/stepdefinition/passagerStepDefinition.java
@@ -3,6 +3,7 @@ package com.speedup.qa.stepdefinition;
 import com.speedup.qa.questions.SelectAdult;
 import com.speedup.qa.questions.SelectBaby;
 import com.speedup.qa.questions.SelectChild;
+import com.speedup.qa.questions.SelectGroup;
 import com.speedup.qa.task.*;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
@@ -10,11 +11,11 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.thucydides.core.annotations.Managed;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
@@ -43,7 +44,6 @@ public class passagerStepDefinition extends PageObject {
 
     @When("^the user clicks the plus button to select more passengers '(\\d+)', '(\\d+)', '(\\d+)'$")
     public void theUserClicksThePlusButtonToSelectMorePassengers(int arg1, int arg2, int arg3) {
-        int total = arg1 + arg2;
 
         for (int i = 1; i < arg1; i++) {
             OnStage.theActorInTheSpotlight().attemptsTo(Adult.adult());
@@ -51,25 +51,22 @@ public class passagerStepDefinition extends PageObject {
 
         for (int i = 0; i < arg2; i++) {
             OnStage.theActorInTheSpotlight().attemptsTo(Child.child());
-            //OnStage.theActorInTheSpotlight().attemptsTo(ChildSecond.childSecond());
         }
 
 
         for (int i = 0; i < arg3; i++) {
             OnStage.theActorInTheSpotlight().attemptsTo(Baby.baby());
-            //OnStage.theActorInTheSpotlight().attemptsTo(BabySecond.babySecond());
-        }
-
-        if (total > 9 || arg1 > 9 || arg2 > 9){
-            Boolean existeElemento = myBrowser.findElements(By.className("v7-rounded v7-rounded--feedback")).size() != 1;
-            System.out.println("It is possible to make a group reservation " + existeElemento);
         }
     }
 
-    @Then("^validate that the number of passengers is well selected$")
-    public void validateThatTheNumberOfPassengersIsWellSelected() {
+    @Then("^validate that the number of passengers is well selected (.*)$")
+    public void validateThatTheNumberOfPassengersIsWellSelected(String reserva) {
         System.out.println("Selected Adults on the website " + SelectAdult.nessageSelectAdult().answeredBy(theActorInTheSpotlight()));
         System.out.println("Selected Children on the website " + SelectChild.messageSelectChild().answeredBy(theActorInTheSpotlight()));
         System.out.println("Selected Babies on the website " + SelectBaby.messageSelectBaby().answeredBy(theActorInTheSpotlight()));
+        if (reserva == reserva) {
+            OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(SelectGroup.messageSelectGroup(), org.hamcrest.Matchers.is(reserva)));
+            System.out.println("It is possible to make a group reservation");
+        }
     }
 }
